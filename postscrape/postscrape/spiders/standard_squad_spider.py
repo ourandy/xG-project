f7470e13f9d0e23124291ea1d8f2dd8db2ddb73d
Andy Lee <andy.mt.lee@gmail.com>
2020-08-14 11:27:11 +0800

    automated squad stats scrape

--- postscrape/postscrape/spiders/standard_squad_spider.py
+++ postscrape/postscrape/spiders/standard_squad_spider.py
@@ -1,43 +1,43 @@
 import scrapy
 from scrapy.selector import Selector
 import re
-
+import pandas as pd
 
 class PostSpider(scrapy.Spider):
 
-    name = 'standard_squads'
+    name = "standard_squads"
 
     start_urls = [
-        'https://fbref.com/en/comps/11/stats/Serie-A-Stats',
+        "https://fbref.com/en/comps/11/stats/Serie-A-Stats",
     ]
 
     def parse(self, response):
 
-        for row in response.xpath('//*[@id="stats_standard_squads"]//tbody/tr'):
-            yield {
-                'team': row.xpath('th/a/text()').extract_first(),
-                'players_used': row.xpath('td[1]//text()').extract_first(),
-                'possession': row.xpath('td[2]//text()').extract_first(),
-                'played': row.xpath('td[3]//text()').extract_first(),
-                'starts': row.xpath('td[4]//text()').extract_first(),
-                'minutes': row.xpath('td[5]//text()').extract_first(),
-                'goals': row.xpath('td[6]//text()').extract_first(),
-                'assists': row.xpath('td[7]//text()').extract_first(),
-                'penalties': row.xpath('td[8]//text()').extract_first(), 
-                'penalties_scored': row.xpath('td[9]//text()').extract_first(),
-                'yellow_cards': row.xpath('td[10]//text()').extract_first(),
-                'red_cards': row.xpath('td[11]//text()').extract_first(),
-                'goals_per_90': row.xpath('td[12]//text()').extract_first(),
-                'assists_per_90': row.xpath('td[13]//text()').extract_first(),
-                'goals_and_assists_per_90': row.xpath('td[14]//text()').extract_first(),
-                'goals_minus_pens_per_90': row.xpath('td[15]//text()').extract_first(),
-                'goals_plus_assists_minus_pens_per_90': row.xpath('td[16]//text()').extract_first(),
-                'xG': row.xpath('td[17]//text()').extract_first(),
-                'npxG': row.xpath('td[18]//text()').extract_first(),
-                'xA': row.xpath('td[19]//text()').extract_first(),
-                'xG90': row.xpath('td[20]//text()').extract_first(),
-                'xA90': row.xpath('td[21]//text()').extract_first(),
-                'xG_plus_xA_90': row.xpath('td[22]//text()').extract_first(),
-                'npxG90': row.xpath('td[23]//text()').extract_first(),
-                'npxG_plus_xA90': row.xpath('td[24]//text()').extract_first()
-            }
+        column_index = 1
+        columns = {}
+        for column_node in response.xpath('//*[@id="stats_standard_squads"]/thead/tr[2]/th'):
+            column_name = column_node.xpath("./text()").extract_first()
+            print("column name is: " + column_name)
+            columns[column_name] = column_index
+            column_index += 1
+            
+            matches = []
+
+        for row in response.xpath('//*[@id="stats_standard_squads"]/tbody/tr'):
+            match = {}
+            for column_name in columns.keys():
+
+                if column_name=='Squad':
+                    match[column_name]=row.xpath('th/a/text()').extract_first()
+                else:
+                    match[column_name] = row.xpath(
+                        "./td[{index}]//text()".format(index=columns[column_name]-1)
+                    ).extract_first()
+
+            matches.append(match)
+        
+        print(matches)
+
+        df = pd.DataFrame(matches,columns=columns.keys())
+
+        yield df.to_csv("test.csv",sep=",", index=False)
--- postscrape/postscrape/spiders/utils.py
+++ postscrape/postscrape/spiders/utils.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import numpy as np
 
-class SpiderUtils():
+
+class SpiderUtils:
     def clean_df(self, df):
         df = df.fillna(value=np.nan)
 
         df.dropna(
             axis=0,
-            how='all',  # use 'any' if you want remove rows with even one empty value
-            inplace=True
+            how="all",  # use 'any' if you want remove rows with even one empty value
+            inplace=True,
         )
         return df
